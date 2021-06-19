d0b177b0d70d864d76f36ba56a12c3084d2704f0
Abelam2001 <85018795+Abelam2001@users.noreply.github.com>
2021-06-19 22:08:15 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,16 +1,31 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-#include <assert.h>
-float average(int [], int);
-
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert ((average(x, 7) - 4.7142) < 0.001);
-
-  int y[] = {5};
-  assert (average(y, 1) == 5.0);
-  printf("Average: passed\n");
+int max(int num[], int size)
+{
+  int max= -999999;
+  for(int i=0;i<size;i++)
+  {
+    if(num[i]>max)
+    {
+      max= num[i];
+    }
   }
+  return max;
+}
+
+int min(int num[], int s)
+{
+  int min= 999999;
+  for(int i=0;i<s;i++)
+  {
+    if(num[i]<min)
+    {
+      min= num[i];
+    }
+  }
+  return min;
+}
+
 float average(int x[], int n){
 float avg=0;
 for(int i=0;i<n;++i){
@@ -18,4 +33,61 @@ avg+=x[i];
 }  
 avg=avg/n;
 return avg;
+}
+
+int mode(int num[], int s)
+{
+  int c[s];
+  int index =0;
+  int max = -999999;
+  
+  for(int x=0;x<s;x++)
+  {
+    c[x]=0;
+  }
+  
+  for(int i =0; i<s; i++)
+  {
+    for(int j=0; j< s; j++)
+    {
+      if (num[i] == num [j])
+      {
+        c[i]++;
+      }
+    }
+  }
+  
+  for(int k= 0;k<s;k++)
+  {
+    if(c[k] > max)
+    {
+      index=k;
+    }
+  }
+  return(num[index]);
+}
+
+int factors(int num, int prf[])
+{
+    int temp=num;
+    int i=0;
+    int pf[]={2,3,5,7,9,11,13,17,19};
+    int prno=(int)(sizeof(pf)/sizeof(int));
+    for(;temp!=1;i++)
+    {
+        for(int j=0;j<prno;j++)
+        {
+            if((temp%pf[j])==0)
+            {
+                prf[i]=pf[j];
+                temp/=pf[j];
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    return i;
 }
